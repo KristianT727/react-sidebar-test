8798b82575bbfcf7d685a74eb2ac87bdc37f0206
Kristian T <kristiantirtawinata@gmail.com>
2023-02-07 11:18:38 +0700

    First initialization

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+function Sidebar() {
+    return <div className="Sidebar"></div>;
+}
+
+export default Sidebar;
---
+++ src/Components/SidebarData.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SidebarData = [
+    {
+        title: "",
+        icon: "",
+        link: "",
+    },
+];
