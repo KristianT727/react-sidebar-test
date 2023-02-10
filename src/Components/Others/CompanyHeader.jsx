d01524a89fb891bf398c583c8d089da5266f47f6
Kristian T <kristiantirtawinata@gmail.com>
2023-02-10 14:37:17 +0700

    navbar completion

--- src/Components/Others/CompanyHeader.jsx
+++ src/Components/Others/CompanyHeader.jsx
@@ -1,9 +1,27 @@
-import "./CompanyHeader.css";
+import style from "./CompanyHeader.module.css";
 
 export const CompanyHeader = () => {
     return (
         <>
-            <div className="header-title"></div>
+            <div className={style.headerTitle}>
+                <div className={style.brandNameContainer}>
+                    <h1>MOTOR CLUB</h1>
+                    <p style={{ color: "#666" }}>
+                        The official website of MOTOR CLUB motor club.
+                    </p>
+                </div>
+                <hr />
+                <div className={style.brandNavbarContainer}>
+                    <ul>
+                        <li>Profile</li>
+                        <li>Visi dan Misi</li>
+                        <li>Produk Kami</li>
+                        <li>Kontak Kami</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <hr />
+            </div>
         </>
     );
 };
