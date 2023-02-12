9f88a6fb8bb06e68ab2f6f5447952491b62636ac
Kristian T <kristiantirtawinata@gmail.com>
2023-02-12 13:51:30 +0700

    Moved product list array to Product page

--- src/Components/ProductComponent/ProductComponent.jsx
+++ src/Components/ProductComponent/ProductComponent.jsx
@@ -1,52 +1,7 @@
 import style from "./ProductComponent.module.css";
 import { ProductCard } from "./ProductCard";
 
-const productListing = [
-    {
-        name: "Helm Berkendara",
-        desc: "Sebuah helm trendy untuk mengendarai motor",
-        imgUrl: "ProductImages/helm.png",
-        parentLink: "",
-        link: "",
-    },
-    {
-        name: "Sepeda Motor",
-        desc: "Sepeda Motor Darley Havidson mulus tanpa kecacatan. Pemilik sebelumnya melakukan perawatan secara berkala teratur.",
-        imgUrl: "ProductImages/bike.png",
-        parentLink: "",
-        link: "",
-    },
-    {
-        name: "Kacamata Berkendara",
-        desc: "Iki kocomoto",
-        imgUrl: "ProductImages/goggles.png",
-        parentLink: "",
-        link: "",
-    },
-    {
-        name: "Oli",
-        desc: "Oli mesin",
-        imgUrl: "ProductImages/oli.png",
-        parentLink: "",
-        link: "",
-    },
-    {
-        name: "Kotak Perkakas",
-        desc: "Kotak perkakas cocok untuk menyimpan peralatan untuk perawatan.",
-        imgUrl: "ProductImages/toolbox.png",
-        parentLink: "",
-        link: "",
-    },
-    {
-        name: "Bakpao isi Ayam",
-        desc: "Bakpao isi ayam yang dibuat oleh grup kelompok, dikukus tetapi memiliki kemungkinan gosong",
-        imgUrl: "ProductImages/bakpao.png",
-        parentLink: "",
-        link: "",
-    },
-];
-
-export const ProductComponent = () => {
+export const ProductComponent = ({ productListing }) => {
     return (
         <div className={style.productListWrap}>
             {productListing.map((prop, key) => {
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,51 @@ import style from "./PagesStyle.module.css";
 import { Helmet } from "react-helmet-async";
 import { ProductComponent } from "../Components/ProductComponent";
 
+const productListing = [
+    {
+        name: "Helm Berkendara",
+        desc: "Sebuah helm trendy untuk mengendarai motor",
+        imgUrl: "ProductImages/helm.png",
+        parentLink: "",
+        link: "",
+    },
+    {
+        name: "Sepeda Motor",
+        desc: "Sepeda Motor Darley Havidson mulus tanpa kecacatan. Pemilik sebelumnya melakukan perawatan secara berkala teratur.",
+        imgUrl: "ProductImages/bike.png",
+        parentLink: "",
+        link: "",
+    },
+    {
+        name: "Kacamata Berkendara",
+        desc: "Iki kocomoto",
+        imgUrl: "ProductImages/goggles.png",
+        parentLink: "",
+        link: "",
+    },
+    {
+        name: "Oli",
+        desc: "Oli mesin",
+        imgUrl: "ProductImages/oli.png",
+        parentLink: "",
+        link: "",
+    },
+    {
+        name: "Kotak Perkakas",
+        desc: "Kotak perkakas cocok untuk menyimpan peralatan untuk perawatan.",
+        imgUrl: "ProductImages/toolbox.png",
+        parentLink: "",
+        link: "",
+    },
+    {
+        name: "Bakpao isi Ayam",
+        desc: "Bakpao isi ayam yang dibuat oleh grup kelompok, dikukus tetapi memiliki kemungkinan gosong",
+        imgUrl: "ProductImages/bakpao.png",
+        parentLink: "",
+        link: "",
+    },
+];
+
 export const Product = () => {
     return (
         <>
@@ -14,7 +59,7 @@ export const Product = () => {
                     <p>Daftar produk-produk yang kami tawarkan.</p>
                 </section>
                 <section>
-                    <ProductComponent />
+                    <ProductComponent productListing={productListing} />
                 </section>
             </div>
         </>
